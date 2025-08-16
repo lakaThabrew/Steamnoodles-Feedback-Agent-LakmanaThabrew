415a04473521fdbae6c9db2d8b9383ba59b3b2f0
lakaThabrew <chulankalakmana@gmail.com>
2025-08-16 09:29:58 +0530

    test#9

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from agents.agent_comment_feedback import run_feedback_agent
 from agents.agent_sentiment_response import run_sentiment_plot_agent
 
 while True:
-    print("\n 🍜 Steam Noodles Feedback Agents")
-    print("1. 📝 Customer Feedback Response Agent")
-    print("2. 📶 Sentiment Visualization Agent")
-    print("3. 🚪 Exit")
+    print("\n")
+    print("+-----------------------------------------------------+")
+    print("|  🌟 Welcome to the Steam Noodles Feedback System 🌟 |")
+    print("+-----------------------------------------------------+")
+    print("| 🍜 Steam Noodles Feedback Agents                    |")
+    print("|    ------------------------------                   |")
+    print("| 1. 📝 Customer Feedback Response Agent              |")
+    print("| 2. 📶 Sentiment Visualization Agent                 |")
+    print("| 3. 🚪 Exit                                          |")
+    print("+-----------------------------------------------------+")
 
     choice = input("\n🔀 Select an option: ")
 
